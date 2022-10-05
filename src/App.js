6efa256dd712c96e8ff4ce8afbf120d6547346db
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-10-05 20:06:17 +0300

    Nimetty html title

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import React, { useState } from 'react';
 
 function WeatherApp() {
 
-
-  const iconUrl= 'http://openweathermap.org/img/wn/';
+  const iconUrl = 'http://openweathermap.org/img/wn/';
   const apiKey = ' copy the api key here';
   const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState('');
   const [icon, setIcon] = useState('');
   const getWeather = (event) => {
     if (event.key === 'Enter') {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=`+apiKey)
-      .then(response => response.json())
-      .then(data => {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=` + apiKey)
+        .then(response => response.json())
+        .then(data => {
 
           setWeatherData(data)
-          setIcon(iconUrl + data.weather[0].icon+'@2x.png')
+          setIcon(iconUrl + data.weather[0].icon + '@2x.png')
           setCity('')
         }
-      )
+        )
     }
   }
   return (
@@ -37,21 +36,21 @@ function WeatherApp() {
       {typeof weatherData.main === 'undefined' ? (
         <div>
           <p>Welcome to weather app! Please enter locality in the box below for current weather information.</p>
-          </div>
-      ): (
+        </div>
+      ) : (
         <div className='weather-data'>
           <p className='city'>{weatherData.name}</p>
           <p className='temp'>{Math.round(weatherData.main.temp)}°C</p>
           <p className='weather'>{weatherData.weather[0].main}</p>
-          <img src={icon} alt= ''/>
-  
+          <img src={icon} alt='' />
+
         </div>
       )}
-    {weatherData.cod === '404' ? (
-      <p>Locality not found.</p>
-    ):(
-      <></>
-    )}
+      {weatherData.cod === '404' ? (
+        <p>Locality not found.</p>
+      ) : (
+        <></>
+      )}
 
     </div>
 
